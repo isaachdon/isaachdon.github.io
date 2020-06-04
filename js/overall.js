651ef07fffe7125a83a4187f2a28ac95be0b9a5a
isaachdon <49210654+isaachdon@users.noreply.github.com>
2020-06-03 21:01:09 -0400

    updated home page

--- js/overall.js
+++ js/overall.js
@@ -10,5 +10,37 @@ $(document).ready(function() {
         }
         lastScrollTop = st;
     });
+
+    //change widths of homepage cards on hover
+    
+    $('#card1').hover(function() {
+        $('#card1').css('width', '50%');
+        $('#card2').css('width', '23%');
+        $('#card3').css('width', '23%');
+    }, function() {
+        $('#card1').css('width', '32%');
+        $('#card2').css('width', '32%');
+        $('#card3').css('width', '32%');
+    });
+    
+    $('#card2').hover(function() {
+        $('#card2').css('width', '50%');
+        $('#card1').css('width', '23%');
+        $('#card3').css('width', '23%');
+    }, function() {
+        $('#card2').css('width', '32%');
+        $('#card1').css('width', '32%');
+        $('#card3').css('width', '32%');
+    });
+    
+    $('#card3').hover(function() {
+        $('#card3').css('width', '50%');
+        $('#card1').css('width', '23%');
+        $('#card2').css('width', '23%');
+    }, function() {
+        $('#card3').css('width', '32%');
+        $('#card1').css('width', '32%');
+        $('#card2').css('width', '32%');
+    });
     
 });
