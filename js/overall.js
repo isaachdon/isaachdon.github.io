b627a2ca06313f67c3ef5fbe1a617728c59d9209
isaachdon <49210654+isaachdon@users.noreply.github.com>
2020-06-03 22:05:00 -0400

    updated homepage and college projects page

--- js/overall.js
+++ js/overall.js
@@ -15,32 +15,32 @@ $(document).ready(function() {
     
     $('#card1').hover(function() {
         $('#card1').css('width', '50%');
-        $('#card2').css('width', '23%');
-        $('#card3').css('width', '23%');
+        $('#card2').css('width', '20%');
+        $('#card3').css('width', '20%');
     }, function() {
-        $('#card1').css('width', '32%');
-        $('#card2').css('width', '32%');
-        $('#card3').css('width', '32%');
+        $('#card1').css('width', '30%');
+        $('#card2').css('width', '30%');
+        $('#card3').css('width', '30%');
     });
     
     $('#card2').hover(function() {
         $('#card2').css('width', '50%');
-        $('#card1').css('width', '23%');
-        $('#card3').css('width', '23%');
+        $('#card1').css('width', '20%');
+        $('#card3').css('width', '20%');
     }, function() {
-        $('#card2').css('width', '32%');
-        $('#card1').css('width', '32%');
-        $('#card3').css('width', '32%');
+        $('#card2').css('width', '30%');
+        $('#card1').css('width', '30%');
+        $('#card3').css('width', '30%');
     });
     
     $('#card3').hover(function() {
         $('#card3').css('width', '50%');
-        $('#card1').css('width', '23%');
-        $('#card2').css('width', '23%');
+        $('#card1').css('width', '20%');
+        $('#card2').css('width', '20%');
     }, function() {
-        $('#card3').css('width', '32%');
-        $('#card1').css('width', '32%');
-        $('#card2').css('width', '32%');
+        $('#card3').css('width', '30%');
+        $('#card1').css('width', '30%');
+        $('#card2').css('width', '30%');
     });
     
 });
